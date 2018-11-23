9211c794ddc7b737cf3e2006b160f66fb7cd7f93
LaN <l.narasimhan@gmail.com>
2018-11-23 05:11:47 +0000

    hello

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ var http = require('http');
 var https = require('https');
 var url = require('url');
 var StringDecoder = require('string_decoder').StringDecoder;
-var config = require('./config');
+var config = require('./lib/config');
 var fs = require('fs');
-
+var handlers = require('./lib/handlers');
+var helpers = require('./lib/helpers');
 
  // Instantiate the HTTP server
 var httpServer = http.createServer(function(req,res){
@@ -73,7 +74,7 @@ var unifiedServer = function(req,res){
         'queryStringObject' : queryStringObject,
         'method' : method,
         'headers' : headers,
-        'payload' : buffer
+        'payload' : helpers.parseJsonToObject(buffer)
       };
 
       // Route the request to the handler specified in the router
@@ -92,27 +93,14 @@ var unifiedServer = function(req,res){
         res.setHeader('Content-Type', 'application/json');
         res.writeHead(statusCode);
         res.end(payloadString);
-        console.log("Returning this response: ",statusCode,payloadString);
-
+        console.log(trimmedPath,statusCode);
       });
 
   });
 };
 
-// Define all the handlers
-var handlers = {};
-
-// Sample handler
-handlers.sample = function(data,callback){
-    callback(406,{'name':'sample handler'});
-};
-
-// Not found handler
-handlers.notFound = function(data,callback){
-  callback(404);
-};
-
 // Define the request router
 var router = {
-  'sample' : handlers.sample
+  'ping' : handlers.ping,
+  'users' : handlers.users
 };
